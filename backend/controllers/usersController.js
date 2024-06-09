21e4ba3df017477b23eebdc5481c61f3689befd7
AlAbedMohammad <mohammad.raedabed@gmail.com>
2024-06-09 20:48:41 +0300

    login function

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-
+const bcrypt = require('bcryptjs');
 exports.register = async (req, res) => {
     const { name, email, password, role } = req.body;
 
@@ -41,3 +41,38 @@ exports.register = async (req, res) => {
     }
 };
 
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ msg: 'Invalid credentials' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: 'Invalid credentials' });
+        }
+
+        const payload = {
+            user: {
+                id: user.id,
+                role: user.role
+            }
+        };
+
+        jwt.sign(
+            payload,
+            process.env.SECRET,
+            { expiresIn: '5h' },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            }
+        );
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
